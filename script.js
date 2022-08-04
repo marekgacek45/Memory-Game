b96bea72343e83f1c5873a9974943ce71610d35c
Marek <marekgacek45@gmail.com>
2022-08-04 07:49:39 +0200

    add function reset

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const cards = document.querySelectorAll('.card')
 
 let hasFlippedCard = false
+let lockBoard = false
 let firstCard
 let secondCard
 
 function flipCard() {
+	if (lockBoard) return
+	if (this === firstCard) return
 	this.classList.add('flip')
 
 	if (!hasFlippedCard) {
@@ -17,18 +20,29 @@ function flipCard() {
 		secondCard = this
 		console.log(secondCard, hasFlippedCard)
 	}
-    matchCard()
+	matchCard()
 }
 
 function matchCard() {
 	if (firstCard.dataset.name === secondCard.dataset.name) {
+		firstCard.removeEventListener('click', flipCard)
+		secondCard.removeEventListener('click', flipCard)
 		console.log('ok')
 	} else {
+        lockBoard = true
 		setTimeout(function () {
 			firstCard.classList.remove('flip')
 			secondCard.classList.remove('flip')
+			reset()
 		}, 1000)
 	}
+}
+
+function reset() {
+	hasFlippedCard = false
+	lockBoard = false
+	firstCard = null
+	secondCard = null
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard))
